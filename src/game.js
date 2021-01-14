7c738375e637bcec744c1fde9040930ae635a205
김진영 <53967482+jin0kim326@users.noreply.github.com>
2021-01-14 18:19:46 +0900

    Refactoring#3 Game

---
+++ src/game.js
@@ -0,0 +1,131 @@
+"use strict";
+
+import Field from "./field.js";
+import * as sound from "./sound.js";
+
+export default class Game {
+  constructor(gameDuration, carrotCount, bugCount) {
+    this.gameDuration = gameDuration;
+    this.carrotCount = carrotCount;
+    this.bugCount = bugCount;
+
+    this.gameBtn = document.querySelector(".game__button");
+    this.gameTimer = document.querySelector(".game__timer");
+    this.gameScore = document.querySelector(".game__score");
+
+    this.gameField = new Field(carrotCount, bugCount);
+    this.gameField.setClickListener(this.onItemClick);
+
+    this.started = false;
+    this.score = 0;
+    this.timer = false;
+
+    this.gameBtn.addEventListener("click", () => {
+      if (this.started) {
+        this.stop();
+      } else {
+        this.start();
+      }
+    });
+  }
+  onItemClick = (item) => {
+    if (!this.started) {
+      return;
+    }
+    if (item === "carrot") {
+      // 당근클릭
+      this.score++;
+      this.updateScoreBoard();
+      if (this.score === this.carrotCount) {
+        this.finish(true);
+      }
+    } else if (item === "bug") {
+      sound.playBug();
+      this.finish(false);
+    }
+  };
+
+  setGameStopListener(onGameStop) {
+    this.onGameStop = onGameStop;
+  }
+
+  start() {
+    this.started = true;
+    this.initGame();
+    this.showStopButton();
+    this.showTimerAndScore();
+    this.startGameTimer();
+    sound.playBackground();
+  }
+
+  stop() {
+    this.started = false;
+    this.stopGameTimer();
+    this.hideGameButton();
+    sound.stopBackground();
+    sound.playAlert();
+    this.onGameStop && this.onGameStop("cancel");
+  }
+
+  finish(win) {
+    this.started = false;
+    this.stopGameTimer();
+    this.hideGameButton();
+    sound.stopBackground();
+    if (win) {
+      sound.playWin();
+    } else {
+      sound.playBug();
+    }
+    this.onGameStop && this.onGameStop(win ? "win" : "lose");
+  }
+
+  showStopButton() {
+    const icon = this.gameBtn.querySelector(".fas");
+    icon.classList.add("fa-stop");
+    icon.classList.remove("fa-play");
+    this.gameBtn.style.visibility = "visible";
+  }
+
+  hideGameButton() {
+    this.gameBtn.style.visibility = "hidden";
+  }
+
+  showTimerAndScore() {
+    this.gameTimer.style.visibility = "visible";
+    this.gameScore.style.visibility = "visible";
+  }
+
+  updateScoreBoard() {
+    this.gameScore.innerText = this.carrotCount - this.score;
+  }
+  startGameTimer() {
+    let remainingTimeSec = this.gameDuration;
+    this.updateTimerText(remainingTimeSec);
+    this.timer = setInterval(() => {
+      if (remainingTimeSec <= 0) {
+        clearInterval(this.timer);
+        this.finish(this.carrotCount === this.score);
+        return;
+      }
+      this.updateTimerText(--remainingTimeSec);
+    }, 1000);
+  }
+
+  stopGameTimer() {
+    clearInterval(this.timer);
+  }
+
+  updateTimerText(time) {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    this.gameTimer.innerText = `${minutes}:${seconds}`;
+  }
+
+  initGame() {
+    //벌레,당근 생성 후 field에 추가
+    this.score = 0;
+    this.gameScore.innerText = this.carrotCount;
+    this.gameField.init();
+  }
+}
--- src/main.js
+++ src/main.js
@@ -2,130 +2,33 @@
 import PopUp from "./popup.js";
 import Field from "./field.js";
 import * as sound from "./sound.js";
+import Game from "./game.js";
 
 const CARROT_COUNT = 10;
 const BUG_COUNT = 10;
 const GAME_DURATION_SEC = 10;
 
-const gameBtn = document.querySelector(".game__button");
-const gameTimer = document.querySelector(".game__timer");
-const gameScore = document.querySelector(".game__score");
+const game = new Game(10, 10, 10);
+game.setGameStopListener((reason) => {
+  let message;
 
-let started = false;
-let score = 0;
-let timer = undefined;
-
-const gameField = new Field(CARROT_COUNT, BUG_COUNT);
-gameField.setClickListener(onItemClick);
-// field.addEventListener("click", onFieldClick);
-
-function onItemClick(item) {
-  if (!started) {
-    return;
+  switch (reason) {
+    case "cancel":
+      message = "Replay ?";
+      break;
+    case "win":
+      message = "YOU WON!!";
+      break;
+    case "lose":
+      message = "YOU LOST..T_T";
+      break;
+    default:
+      throw new Error("not valid reason");
   }
-  if (item === "carrot") {
-    // 당근클릭
-    score++;
-    updateScoreBoard();
-    if (score === CARROT_COUNT) {
-      finishGame(true);
-    }
-  } else if (item === "bug") {
-    sound.playBug();
-    finishGame(false);
-  }
-}
+  gameFinishBanner.showWithText(message);
+});
 
 const gameFinishBanner = new PopUp();
 gameFinishBanner.setClickListener(() => {
-  startGame();
+  game.start();
 });
-
-function startGame() {
-  started = true;
-  sound.playBackground();
-  initGame();
-  showStopButton();
-  showTimerAndScore();
-  startGameTimer();
-}
-
-function stopGame() {
-  started = false;
-  sound.stopBackground();
-  sound.playAlert();
-  stopGameTimer();
-  hideGameButton();
-
-  gameFinishBanner.showWithText("REPLAY ?");
-}
-
-function finishGame(win) {
-  started = false;
-  sound.stopBackground();
-  stopGameTimer();
-  hideGameButton();
-  if (win) {
-    sound.playWin();
-  } else {
-    sound.playBug();
-  }
-  gameFinishBanner.showWithText(win ? "You Win!!" : "You Lost...T_T");
-}
-
-gameBtn.addEventListener("click", () => {
-  if (started) {
-    stopGame();
-  } else {
-    startGame();
-  }
-});
-
-function showStopButton() {
-  const icon = gameBtn.querySelector(".fas");
-  icon.classList.add("fa-stop");
-  icon.classList.remove("fa-play");
-  gameBtn.style.visibility = "visible";
-}
-
-function hideGameButton() {
-  gameBtn.style.visibility = "hidden";
-}
-
-function showTimerAndScore() {
-  gameTimer.style.visibility = "visible";
-  gameScore.style.visibility = "visible";
-}
-
-function updateScoreBoard() {
-  gameScore.innerText = CARROT_COUNT - score;
-}
-function startGameTimer() {
-  let remainingTimeSec = GAME_DURATION_SEC;
-  updateTimerText(remainingTimeSec);
-  timer = setInterval(() => {
-    if (remainingTimeSec <= 0) {
-      clearInterval(timer);
-      finishGame(CARROT_COUNT === score);
-      return;
-    }
-    updateTimerText(--remainingTimeSec);
-  }, 1000);
-}
-
-function stopGameTimer() {
-  clearInterval(timer);
-}
-
-function updateTimerText(time) {
-  const minutes = Math.floor(time / 60);
-  const seconds = time % 60;
-  gameTimer.innerText = `${minutes}:${seconds}`;
-}
-
-function initGame() {
-  //벌레,당근 생성 후 field에 추가
-  score = 0;
-  gameScore.innerText = CARROT_COUNT;
-  gameField.init();
-}
